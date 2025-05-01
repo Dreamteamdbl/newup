2b84274741277b4694b69a3c6e8f0d8ca38ed4d4
Dreamteamdbl <amazingyaan@gmail.com>
2025-05-01 11:33:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -402,11 +402,11 @@ async def account_login(bot: Client, m: Message):
     await input.delete(True)
     file_name, ext = os.path.splitext(os.path.basename(y)  # Extract filename & extension
 
-    if file_name.startswith("Naruto://"):  # ✅ Check if filename ends with "_helper"
-        x = decrypt_file_txt(y)  # Decrypt the file
-        await input.delete(True)
-    else:
-        x = y 
+    #if file_name.startswith("Naruto://"):  # ✅ Check if filename ends with "_helper"
+        #x = decrypt_file_txt(y)  # Decrypt the file
+        #await input.delete(True)
+    #else:
+        #x = y 
 
 
     path = f"./downloads/{m.chat.id}"
